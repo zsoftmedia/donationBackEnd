b698b8678d72b6f51e8427cd844bac5dd0a8ba00
Zahaib Ali <53561736+zsoftmedia@users.noreply.github.com>
2025-03-13 13:32:39 +0100

    add new object

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -41,7 +41,7 @@ router.get("/transactions", async (req, res) => {
       customers: customers, 
     });
   } catch (error) {
-    console.error("❌ Error fetching transactions:", error);
+    console.error("Error fetching transactions:", error);
     res.status(500).json({ error: error.message });
   }
 });
@@ -49,25 +49,12 @@ router.get("/transactions", async (req, res) => {
 
 router.post("/create-checkout-session", async (req, res) => {
   try {
-    console.log("✅ Request received at /api/payment/create-checkout-session");
-    console.log("📩 Received Data:", req.body); // ✅ Log the received data
-
-    // Extract request body
     const { name, email, address, amount, phone } = req.body;
-
-    // ✅ Validate required fields before making Stripe API call
-    if (!name || !email || !amount || !phone) {
-      console.error("❌ Missing required fields:", req.body);
-      return res.status(400).json({ error: "Missing required fields" });
-    }
-
-    console.log("🛒 Creating Stripe checkout session for:", name, email, amount);
-
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ["card", "klarna", "eps"], // ✅ Added EPS
+      payment_method_types: ["card", "klarna", "eps"],
       automatic_payment_methods: { enabled: true },
       customer_email: email,
-      phone_number_collection: { enabled: true },
+      phone_number_collection: { enabled: true, },
       billing_address_collection: "required",
       mode: "payment",
       line_items: [
@@ -75,20 +62,16 @@ router.post("/create-checkout-session", async (req, res) => {
           price_data: {
             currency: "eur",
             product_data: { name: "Donation" },
-            unit_amount: parseInt(amount) * 100, // ✅ Convert to cents and ensure integer
+            unit_amount: parseInt(amount) * 100, 
           },
           quantity: 1,
         },
       ],
       metadata: { name, email, phone, address },
-      success_url: "http://localhost:3000/success",
-      cancel_url: "http://localhost:3000/cancel",
+      success_url: `https://ghb-clanstvo.netlify.app/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `https://ghb-clanstvo.netlify.app/`, 
     });
-
-    console.log("✅ Stripe session created:", session.url);
-    return res.json({ url: session.url });
   } catch (error) {
-    console.error("❌ Stripe Error:", error);
     return res.status(500).json({ error: "Payment session could not be created." });
   }
 });
